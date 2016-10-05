5b490bb9667799d284eda8c0d1139f322e3de9c0
Eric Torunski <etorunski@gmail.com>
2016-10-04 22:35:55 -0400

    Reworked the MainActivity Interface to be an ArrayAdapter<String>

---
+++ app/src/main/java/com/example/etorunski/inclassexamples/SensorActivity.java
@@ -0,0 +1,10 @@
+package com.example.etorunski.inclassexamples;
+
+import android.app.Activity;
+
+/**
+ * Created by etorunski on 2016-10-04.
+ */
+
+public class SensorActivity extends Activity {
+}
