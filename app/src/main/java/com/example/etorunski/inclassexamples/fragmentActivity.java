f57ba494e04f2fdbba8fb02314dd39d324ffbe97
Eric Torunski <etorunski@gmail.com>
2016-11-08 23:28:09 -0500

    Added comments to fragments example

--- app/src/main/java/com/example/etorunski/inclassexamples/fragmentActivity.java
+++ app/src/main/java/com/example/etorunski/inclassexamples/fragmentActivity.java
@@ -17,22 +17,28 @@ public class fragmentActivity extends Activity {
         setContentView(R.layout.activity_fragment);
         boolean isTablet = findViewById(R.id.fragmentspace) != null;
 
+
+        //If this is a tablet, then the fragment is on the same screen so load it.
         if(isTablet)
         {
             final info_fragment inf = new info_fragment();
+            inf.setMessage("Set the message for the fragment");
+
             getFragmentManager().beginTransaction()
                     .add(R.id.fragmentspace, inf).commit();
 
+            //Set button 1 to remove the fragment
             Button b = (Button)findViewById(R.id.button1);
             b.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    inf.setMessage(("New message here"));
-                    getFragmentManager().beginTransaction()
-                            .remove( inf ).commit();
+                    //Using the builder pattern to remove the loaded fragment
+                    getFragmentManager().beginTransaction().remove( inf ).commit();
                 }
             });
 
+            //set button to add a fragment to the empty FrameLayout - id.fragmentspace
+            // This is the builder pattern:
             b = (Button)findViewById(R.id.button2);
             b.setOnClickListener(new View.OnClickListener() {
                 @Override
@@ -43,12 +49,13 @@ public class fragmentActivity extends Activity {
             });
 
         }
-        else
+        else    //This is running on a phone. This layout just has 3 buttons.
         {
             Button b = (Button)findViewById(R.id.button1);
             b.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    //If the user clicked button 1, start a new activity with just the fragment
                     startActivity(new Intent(fragmentActivity.this, phone_details.class));
                 }
 
